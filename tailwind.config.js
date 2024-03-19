43bbed585b07639fd6f81ef5c289343239bc2f6d
stanley-meruo <meruostanleovic86@gmail.com>
2024-03-19 08:21:46 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {
+      backgroundImage: {
+        'header-A-bg': "url('/images/demo-hosting-header-bg.jpg')",
+        'header-B-bg': "url('/images/demo-hosting-home-bg.jpg')",
+        'footer-bg': "url('/images/demo-hosting-footer-bg.jpg')",
+        'section-A-bg': "url('.images/demo-hosting-home-06.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
+
